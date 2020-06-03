b80af8b4b37c4da2851d0c79a268fbf665de8db6
xCito <juanlantiguajr@gmail.com>
2020-06-03 17:48:13 -0400

    Added command history navigation with arrow keys

--- code.js
+++ code.js
@@ -8,7 +8,6 @@ class GitCommandProccessor {
         cmd = cmd.replace(/\s+/," ");
         let tokens = cmd.split(/\s/).filter(s => s);
         let response = "";
-        console.log(tokens);
         
         if(this.startsWithGit(tokens[0])) {
             response = `${cmd}\n\tThis is custom CitoGit\n\tversion - 1.0.0\n\n`;
@@ -45,7 +44,6 @@ class GitCommandProccessor {
     startsWithGit(cmd) {
         const startsWithGit = new RegExp(/^git\s?/);
         let result = startsWithGit.exec(cmd);
-        console.log(result);
         return result ? true : false;
     }
 
@@ -96,29 +94,56 @@ class Terminal {
         this.gitProcessor = new GitCommandProccessor();
         this.prefix = 'xCito\\home> ';
         this.setUpEventListeners();
+        this.commandHistory = [""];
+        this.historyIndex = 0;
     }
 
     setUpEventListeners() {
-        this.#commandLineElem.addEventListener('keypress', (e) => this._handleEnterKey(e));
+        this.#commandLineElem.addEventListener('keydown', (e) => this._handleKeyPress(e));
         this.#terminalElem.addEventListener('click', () => this._focusOnInputElem());
     }
 
-    _handleEnterKey(e) {
+    _handleKeyPress(e) {
         if ('Enter' === e.key) {    
             let cmd = this.#commandLineElem.value;
-            let output = "";
-
+            let output;
+            
             if(cmd == "") {
-                this._addToFeed(output);
+                this._addToFeed("");
                 return;
             }
-
 
             output = this.processCommand(cmd);
             if(output != null) {
                 this._addToFeed(output);
             }
+        } else if ('ArrowUp' === e.key) {
+            this.navigateCommandHistory(1);
+        } else if ('ArrowDown' === e.key) {
+            this.navigateCommandHistory(-1);
         }
+    }
+
+    addToCommandHistory(cmd) {
+        this.commandHistory.splice(1,0,cmd);
+        if(this.commandHistory.length > 15) {
+            this.commandHistory.pop();
+        }
+    }
+
+    navigateCommandHistory(dir) {
+        if(this.commandHistory.length === 0) {
+            return;
+        }
+        this.historyIndex += dir;
+
+        if( this.historyIndex < 0 ) {
+            this.historyIndex = 0;
+        }
+        if( this.historyIndex >= this.commandHistory.length ) {
+            this.historyIndex = this.commandHistory.length-1;
+        }
+        this.#commandLineElem.value = this.commandHistory[ this.historyIndex ];
     }
     
     processCommand(cmd) {
@@ -127,10 +152,14 @@ class Terminal {
             case 'clear':
                 this.clearTerminal();
                 break;
+            case 'help':
+                this.displayHelpOptions();
+                break;
             default:
                 msg = this.gitProcessor.process(cmd);
                 break;
         }
+        this.addToCommandHistory(cmd);
         return msg;
     }
 
@@ -141,14 +170,28 @@ class Terminal {
         this.#commandLineElem.focus();
     }
 
+    displayHelpOptions() {
+        let output = "\n Terminal Commands\n" +
+                     "    help\t\t\t*shows this menu*\n" +
+                     "    clear\t\t\t*clears the terminal*\n" + 
+                     "\n xCitoGit Commands\n" +
+                     "    git branch\t\t\t*displays all branches*\n" +
+                     "    git branch <branchName>\t*creates a new branch*\n" +
+                     "    git checkout <branchName>\t*switches to <branchName>*\n" +
+                     "    git rebase <branchName>\t*noactionyet*\n" +
+                     "    git merge <branchName>\t*noactionyet*\n\n";
+                     
+        this._addToFeed(output);
+    }
+
     _focusOnInputElem() {
         this.#commandLineElem.focus();   
     }
 
-    _addToFeed( html ) {
+    _addToFeed( text ) {
         let span = document.createElement('span');
         span.classList.add('terminal-feed-entry');
-        span.innerHTML = this.prefix + html;
+        span.innerText = this.prefix + text;
         this.#commandLineElem.value = '';
 
         this.#terminalFeedElem.appendChild(span);
