f0c01d6f66253dff7601ed62cafd58a0b5a36192
xCito <juanlantiguajr@gmail.com>
2020-06-22 00:59:41 -0400

    completed adding the rebase command

--- code.js
+++ code.js
@@ -1,9 +1,16 @@
 class Commit {
     constructor(msg) {
+        this.id = this.createId();
         this.message = msg;
         this.prev = null;
         this.next = null;
         this.branchCommits = [];
+    }
+
+    createId() {
+        let randNum = Math.floor(((Math.random() * 899999) + 100000) * 100);
+        randNum += Math.floor(Math.random() * 10);
+        return randNum.toString(16);
     }
 }
 
@@ -63,7 +70,7 @@ class GitCommandProcessor {
                     response = `${cmd}\n\tMerge things`;
                     break; 
                 case 'rebase':
-                    response = `${cmd}\n\tRebase things`;
+                    response = `${cmd}\n\t${this.rebaseCommand(tokens.slice(2))}`;
                     break;
                 case 'checkout':
                     response = `${cmd}\n${this.checkoutCommand(tokens[2])}`;
@@ -95,8 +102,12 @@ class GitCommandProcessor {
             }, "");
             resp = `All Branches:\n${branchNames}\n`;
         } else {
-            this.branches.push(new Branch(arg, this.curBranch.curCommit));
-            resp = `Created branch '${arg}'\n`;
+            if(!this.doesBranchExist(arg)) {
+                this.branches.push(new Branch(arg, this.curBranch.curCommit));
+                resp = `Created branch '${arg}'\n`;
+            } else {
+                resp = `Branch with name '${arg}' already exists\n\tno new branch created.\n\n`;
+            }
         }
 
         return resp;
@@ -105,9 +116,8 @@ class GitCommandProcessor {
     checkoutCommand(arg) {
         let resp;
         if (arg) {
-            let branchExists = this.branches.some( b => b.name === arg); 
-            if (branchExists) {
-                this.curBranch = this.branches.find( b => b.name === arg);
+            if (this.doesBranchExist(arg)) {
+                this.curBranch = this.getBranchByName(arg);
                 console.log('now the current branch is ' + this.curBranch.name);
                 resp = `\tSwitched the '${arg}' branch.\n`;
             } else {
@@ -142,43 +152,125 @@ class GitCommandProcessor {
         return resp;
     }
 
+    rebaseCommand(args) {
+        let resp;
+
+        if (this.doesBranchExist(args[0])) {
+            let rebaseOntoThisBranch = this.getBranchByName(args[0]);
+
+            if(this.curBranch.name !== rebaseOntoThisBranch.name) {
+                let commit = this.findCommonAncestor(this.curBranch, rebaseOntoThisBranch);
+                let isFromSameCommitTree = commit.branchCommits.length === 0 || commit.branchCommits.some(c => this.findBranchWithThisCommit(c).name !== this.curBranch.name);
+
+                if(!isFromSameCommitTree) {                
+                    // Remove from branch commits list
+                    let branchBaseCommit; 
+                    commit.branchCommits = commit.branchCommits.filter(commit => {
+                        if (this.findBranchWithThisCommit(commit).name !== this.curBranch.name)
+                            return true;
+
+                        branchBaseCommit = commit;
+                        return false;                  
+                    });
+                    console.log('base commit for %s, \n %o', this.curBranch.name, branchBaseCommit);
+
+                    // set base of current branch on top of 'arg branch'
+                    branchBaseCommit.prev = rebaseOntoThisBranch.curCommit; // rebaseC <-- newCom
+                    rebaseOntoThisBranch.curCommit.branchCommits.push(branchBaseCommit); // rebaseC --> newCom
+
+                    resp = `Rebased ${this.curBranch.name} branch onto ${rebaseOntoThisBranch.name} branch\n`;
+                 } else {
+                    resp = `Rebase failed, both ${this.curBranch.name} and ${rebaseOntoThisBranch.name} are in the same commit tree.\n`;
+                }
+            } else {
+                resp = `Rebase failed, cannot rebase onto self\n`;
+            }
+        } else {
+            resp = `Branch '${args}' doesnt exist.\n`;
+        }
+
+        return resp;
+    }
+
     /**
-     * @param branch1 - Branch
-     * @param branch2 - Branch
+     * @return {boolean}
      */
-    static findCommonAncestor(branch1, branch2) {
+    doesBranchExist(branchName) {
+        console.log(branchName);
+        console.log(this.branches);
+        console.log(this.branches.some( b => {
+            console.log(b.name, branchName);
+            return b.name === branchName;
+        }));
+        return this.branches.some( b => b.name === branchName);
+    }
+
+    /**
+     * @return {Branch}
+     */
+    getBranchByName(branchName) {
+        return this.branches.find( b => b.name === branchName);
+    }
+
+    /**
+     * This function finds the first commit both 
+     * branches have in common. 
+     * @param {Branch} branch1 - A git branch
+     * @param {Branch} branch2 - Another git branch
+     * @return {Commit} The branches' common commit or null if non found
+     */
+    findCommonAncestor(branch1, branch2) {
         const set = new Set();
         let commit1 = branch1.curCommit;
         let commit2 = branch2.curCommit;
 
         // Both branches are pointing to same commit.
-        if(commit1 === commit2) {
+        if(commit1.id == commit2.id) {
             return commit1;
         }
     
         while(commit1 !== null || commit2 !== null) {
             if(commit1 !== null) { 
-                if(!set.has(commit1)) {
-                    set.add(commit1);
+                if(!set.has(commit1.id)) {
+                    set.add(commit1.id);
                     commit1 = commit1.prev;
                 } else return commit1;
             }
             if(commit2 !== null) { 
-                if(!set.has(commit2)) { 
-                    set.add(commit2);
+                if(!set.has(commit2.id)) { 
+                    set.add(commit2.id);
                     commit2 = commit2.prev;
                 } else return commit2;
             }
         }
+ 
         return null;    
     }
 
+    findBranchWithThisCommit(commit) {
+        let cur = commit;
+        
+        while(cur.next !== null) {
+            cur = cur.next;
+        }
+
+        let result = this.branches.filter(branch => branch.curCommit.id === cur.id);
+        return result[0];
+    }
+
+    /**
+     * Debug function to display the commit tree.
+     * DFS approach starting with main commit tree line, then branched
+     * off commits.
+     * @param {Commit} - a root commit
+     */
     displayCommits(cur) {
         if (cur === null || cur === undefined) {
             return;
         }
-    
-        console.log(cur.message + "\t| \tprev: " + cur.prev?.message + "\t|\tnext: " + cur.next?.message + "\t|\tbranchoffs: " + cur.branchCommits.map(c=>c.message));
+        
+        let b = this.branches.filter( b => b.curCommit.id === cur.id )[0];
+        console.log('id=%s | msg=%s | next=%s | prev=%s | bCommits[%s] %s', cur.id, cur.message, cur.next?.message, cur.prev?.message, cur.branchCommits.map(c=>c.message).join(','), b ? ' <---'+b.name+' branch':'');
         this.displayCommits(cur.next);
         
         for(var i=0; i<cur.branchCommits.length; i++) {
@@ -358,11 +450,7 @@ feature3.addNewCommit('b1', false);
 feature3.addNewCommit('b2', false);
 feature3.addNewCommit('b3', false);
 let cur = root;
-// console.log(cur);
-// while(cur != null) {
-//     console.log(cur.message + "\t| \tprev: " + cur.prev?.message + "\t|\tnext: " + cur.next?.message + "\t|\tbranchoffs: " + cur.branchCommits.map(c=>c.message));
-//     cur = cur.next;
-// }
+
 
 function displayCommits(cur) {
     if (cur === null || cur === undefined) {
@@ -378,5 +466,3 @@ function displayCommits(cur) {
 }
 
 displayCommits(root);
-
-console.log(GitCommandProcessor.findCommonAncestor(feature3, feature2));
