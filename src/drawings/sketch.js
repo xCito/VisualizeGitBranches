b425b5d123b0f694e7839bd89bb91b2d90aa9170
xCito <juanlantiguajr@gmail.com>
2020-08-08 19:51:09 -0400

    refactored file structure again and incorporated p5js

---
+++ src/drawings/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    let canvas = createCanvas(400, 400);
+    canvas.parent('canvas-container');
+}
+  
+function draw() {
+if (mouseIsPressed) {
+    fill(0);
+} else {
+    fill(255);
+}
+ellipse(mouseX, mouseY, 80, 80);
+}
